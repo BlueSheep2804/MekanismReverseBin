5fa6ab6093b0619b3fb46a70d64ae755573c52f4
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-08-25 14:46:37 +0900

    feat: 🎸 ビンの取り出し動作を反転するmixinを追加

---
+++ src/main/java/dev/bluesheep/mekanisminvertbin/mixin/BlockBinMixin.java
@@ -0,0 +1,17 @@
+package dev.bluesheep.mekanisminvertbin.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import mekanism.common.block.basic.BlockBin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BlockBin.class)
+public class BlockBinMixin {
+    @ModifyExpressionValue(
+            method = "attack",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isShiftKeyDown()Z")
+    )
+    private boolean invertSneakAttack(boolean original) {
+        return !original;
+    }
+}

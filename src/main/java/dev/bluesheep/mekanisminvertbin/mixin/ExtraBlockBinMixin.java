45cf2bd82eb54602218f9586ead59e7505617bae
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-08-25 22:38:53 +0900

    feat: 🎸 Mekanism Extrasのビンに対応

---
+++ src/main/java/dev/bluesheep/mekanisminvertbin/mixin/ExtraBlockBinMixin.java
@@ -0,0 +1,17 @@
+package dev.bluesheep.mekanisminvertbin.mixin;
+
+import com.jerry.mekanism_extras.common.block.basic.ExtraBlockBin;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ExtraBlockBin.class)
+public class ExtraBlockBinMixin {
+    @ModifyExpressionValue(
+            method = "attack",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isShiftKeyDown()Z")
+    )
+    private boolean invertSneakAttack(boolean original) {
+        return !original;
+    }
+}
